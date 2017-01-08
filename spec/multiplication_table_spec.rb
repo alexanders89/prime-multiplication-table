2b55a77525d24b4b835f8604d2456aaa659faa97
Coleby Kent <coleby210@gmail.com>
2017-01-07 22:25:35 -0800

    Add tests to ensure proper behavior of the Multiplication Table Class

--- spec/multiplication_table_spec.rb
+++ spec/multiplication_table_spec.rb
@@ -1,5 +1,17 @@
 require_relative '../lib/multiplication_table.rb'
 
 describe MultiplicationTable do
-  it 'outputs a prime multiplication table to a matrix'
+  let(:three) { [2, 3, 5] }
+
+  it 'is a Multiplication Table Object' do
+    expect(MultiplicationTable.new(three, three)).to be_a MultiplicationTable
+  end
+
+  it 'can transform a prime multiplication table to a matrix' do
+    expect(MultiplicationTable.new(three, three).table).to eq([[nil, 2, 3, 5], [2, 4, 6, 10], [3, 6, 9, 15], [5, 10, 15, 25]])
+  end
+
+  it 'can transform any set of numbers in an array to a matrix' do
+    expect(MultiplicationTable.new([1, 2, 3], [4, 5, 6]).table).to eq([[nil, 1, 2, 3], [4, 4, 8, 12], [5, 5, 10, 15], [6, 6, 12, 18]])
+  end
 end
