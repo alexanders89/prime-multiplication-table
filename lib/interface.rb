ac4383d795066bba24e52e28507c2dcf447fdf48
Coleby Kent <coleby210@gmail.com>
2017-01-08 09:49:23 -0800

    Seperate code from the runner file into it's own interface class and file

---
+++ lib/interface.rb
@@ -0,0 +1,33 @@
+require_relative 'prime.rb'
+require_relative 'multiplication_table.rb'
+require 'terminal-table'
+
+# Output Class to properly handle output
+class Interface
+  def self.help
+    puts 'Welcome to the Primes Multiplication Table Help Menu!'
+    puts "Use '-s' to set the amount of primes"
+    puts 'Example: ruby runner.rb -s 15'
+  end
+
+  def self.size(num_string)
+    if num_string.is_i?
+      table(num_string.to_i)
+    else
+      puts num_string + ' is not a number'
+      puts 'Please enter a valid number'
+    end
+  end
+
+  def self.table(num = 10)
+    primes = Prime.first(num)
+    puts Terminal::Table.new rows: MultiplicationTable.new(primes, primes).table
+  end
+end
+
+# Add is_i? method to the String Class
+class String
+  def is_i?
+    /\A[-+]?\d+\z/ === self
+  end
+end
--- runner.rb
+++ runner.rb
@@ -1,27 +1,11 @@
-require_relative 'lib/prime.rb'
-require_relative 'lib/multiplication_table'
-require 'terminal-table'
-
-def int_check(string)
-  /\A[-+]?\d+\z/ === string
-end
+require_relative 'lib/interface.rb'
 
 if ARGV.any?
   if ARGV[0] == 'help' || ARGV[0] == '-h'
-    puts 'Welcome to the Primes Multiplication Table Help Menu!'
-    puts "Use '-s' to set the amount of primes"
-    puts 'Example: ruby runner.rb -s 15'
+    Interface.help
   elsif ARGV[0] == 'size' || ARGV[0] == '-s'
-    if int_check(ARGV[1])
-      primes = Prime.first(ARGV[1].to_i)
-      table = Terminal::Table.new rows: MultiplicationTable.new(primes, primes).table
-      puts table
-    else
-      puts ARGV[1] + ' is not a number'
-      puts 'Please enter a valid number'
-    end
+    Interface.size(ARGV[1])
   end
 else
-  table = Terminal::Table.new rows: MultiplicationTable.new(Prime.first, Prime.first).table
-  puts table
+  Interface.table
 end
